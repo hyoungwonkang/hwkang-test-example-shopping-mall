e050c1245393573e85f5e770d5075698cfbb0851
hyoungwonkang <hwplus@gmail.com>
2024-10-11 16:50:34 +0900

    jsdom 구조 알기 위한 debug

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -7,8 +7,13 @@ import render from '@/utils/test/render';
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   await render(<TextField className="my-class" />);
 
-  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-    'my-class',
-  );
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  screen.debug();
+
+  expect(textInput).toHaveClass('my-class');
+  // // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인
+  // expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+  //   'my-class',
+  // );
 });
