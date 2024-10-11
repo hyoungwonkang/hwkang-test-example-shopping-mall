1955743e755b2aaf37f82be1f6ef794c6315fb1a
hyoungwonkang <hwplus@gmail.com>
2024-10-11 15:27:59 +0900

    렌더링되는 돔구조 올바르게 변경되었는지 확인

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,11 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  await render(<TextField className="my-class" />);
+
+  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
 });
