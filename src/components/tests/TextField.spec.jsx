cd265173363fa8253dfe857c844815e7d5c64064
hyoungwonkang <hwplus@gmail.com>
2024-10-11 17:09:43 +0900

    테스트 환경과 매처

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -12,8 +12,27 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   screen.debug();
 
   expect(textInput).toHaveClass('my-class');
-  // // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인
-  // expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-  //   'my-class',
-  // );
+});
+
+// it은 단위
+// it -> test 함수의 alias. test(), describe() 같은 함수가 있음.
+describe('placeholder', () => {
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    //js DOM에 반영
+    await render(<TextField />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
+    // 매처. toBeInTheDocument()도 매처. 공식문서 참고.
+    expect(textInput).toBeInTheDocument();
+  });
+
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    await render(<TextField placeholder="상품명을 입력해 주세요." />);
+
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    expect(textInput).toBeInTheDocument();
+  });
 });
